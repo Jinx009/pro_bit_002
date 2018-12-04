d42667e045f2ba6f3b96d7fa372dfcee25b1213b
Jinx <jinxlovejinx@vip.qq.com>
2018-12-04 14:29:05 +0800

    project init

---
+++ pages/confirm/confirm.js
@@ -0,0 +1,42 @@
+Page({
+  goList: function () {
+    wx.redirectTo({
+      url: '/pages/list/list'
+    })
+  },
+  onChange(e) {
+    console.log('onChange', e)
+    this.setData({
+      error: isTel(e.detail.value),
+      value: e.detail.value,
+    })
+  },
+  onFocus(e) {
+    this.setData({
+      error: isTel(e.detail.value),
+    })
+    console.log('onFocus', e)
+  },
+  onBlur(e) {
+    this.setData({
+      error: isTel(e.detail.value),
+    })
+    console.log('onBlur', e)
+  },
+  onConfirm(e) {
+    console.log('onConfirm', e)
+  },
+  onClear(e) {
+    console.log('onClear', e)
+    this.setData({
+      error: true,
+      value: '',
+    })
+  },
+  onError() {
+    wx.showModal({
+      title: 'Please enter 11 digits',
+      showCancel: !1,
+    })
+  },
+})
